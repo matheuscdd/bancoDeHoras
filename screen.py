012d3318457849468b1747df73a6bf245fabe4e4
Claus <matheuscdd@hotmail.com>
2023-03-15 07:02:40 -0300

    feat: change

--- screen.py
+++ screen.py
@@ -1,13 +1,48 @@
 from tkinter import *
 
 win = Tk()
-win.title("Banco de horas")
-win.iconbitmap("heart.ico")
+win.resizable(False, False)
+
+condition = dict(
+    title="Banco de horas",
+    btn="Carregando...",
+    icon="clock.ico",
+    work=False
+)
+
+win.iconbitmap(condition["icon"])
+
+def working():
+    win.title("Trabalhando...")
+    win.iconbitmap("yes.ico")
+    button["text"] = "Check-out"
+
+
+def pause():
+    win.title("Pausa")
+    win.iconbitmap("not.ico")
+    button["text"] = "Check-in"
+
+
+
+def status():
+    condition["work"] = not condition["work"]
+    working() if condition["work"] else pause()
+
+
+title = Label(win, text=condition["title"])
+title.grid(column=0, row=0, pady=20, padx=20)
+button = Button(win, text=condition["btn"], command=status)
+button.grid(column=0, row=2, padx=10, pady=10)
+
+text = Text(win, height=4, width=30, font="Calibri 10", padx=10, pady=10)
+text.insert(INSERT, "oLÁ \n")
+text.insert(END, "Acabou")
+text.grid(column=0, row=1)
+
 
 title = Label(win, text="Banco de horas")
 title.grid(column=0, row=0, pady=20, padx=20)
-#melhorar esse cara
-button = Button(win, text="check-in", command=lambda: win.iconbitmap("clock.ico"))
-button.grid(column=0, row=1, padx=10, pady=10)
+
 
 win.mainloop()
